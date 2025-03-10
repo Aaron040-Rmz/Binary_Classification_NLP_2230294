adee9278bd7fed4c3ab833cf6577fcfebf2f4c8d
Aaron040-Rmz <Aaron.rmz122@gmail.com>
2025-03-09 23:57:07 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Importar la función de entrenamiento y evaluación desde el módulo neural_networks_keras
+from src.Binary_Classification_NLP import binary_classification
+
+# Verifica si el script se ejecuta directamente
+if __name__ == "__main__":
+   binary_classification()  # Llama a la función para entrenar y evaluar la red neuronal
